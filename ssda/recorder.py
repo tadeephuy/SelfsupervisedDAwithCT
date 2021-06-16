7a6115d4ea94098851fee0620db3bb2525d656e5
tks1998 <37293650+tks1998@users.noreply.github.com>
2021-06-16 15:36:05 +0700

    Create tensordboard (#4)
    * add function visualization
    
    * delete .idea

--- ssda/recorder.py
+++ ssda/recorder.py
@@ -1,13 +1,20 @@
 from collections import defaultdict
 from copy import deepcopy
 import pandas as pd
+import os
+import tensorflow as tf
+import datetime
 
 class Recorder:
     def __init__(self, tags=['train_batch', 'train_epoch', 'valid_epoch'], name='record'):
         self.tags = tags
         self.logs = {k: defaultdict(lambda: defaultdict(lambda: AverageMeter())) for k in self.tags}
         self.name = name
-        self.logs_df = None
+        self.logs_df = 'logs'
+        os.makedirs(self.logs_df, exist_ok=True)
+        current = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+        self.tbwriter = {tag: tf.summary.create_file_writer('./logs/' + current + '/' + tag) for tag in tags}
+        self.index = {}
 
     def update(self, log, tag):
         
@@ -19,8 +26,31 @@ class Recorder:
                 for name in v.keys():
                     self.logs[tag][k][name] = log[k][name] # metrics is updated by replacement
 
-    def visualize(self, *args):
-        pass        
+    def visualize(self, tag, *args):
+        def calc_index(self, graph_name):
+            if graph_name not in self.index.keys():
+                self.index[graph_name] = 0
+            else:
+                self.index[graph_name] += 1
+            return self.index[graph_name]
+
+        with self.tbwriter[tag].as_default():
+            if tag in ['train_batch']:
+                for type_ in ['losses', 'metrics']:
+                    for name, value in self.logs[tag][type_].items():  # metric name
+                        graph_name = '{}/{}/{}'.format(tag, type_, name)
+                        tf.summary.scalar(graph_name, value.avg, step=calc_index(self, graph_name))
+            elif tag in ['train_epoch', 'valid_epoch']:
+                for type_ in ['losses', 'metrics']:
+                    graph_name = '{}/{}'.format(tag, type_)
+                    try:
+                        ### type(metrics) is dictionary
+                        for name, value in self.logs[tag][type_].items():
+                            graph_name = '{}/{}/{}'.format(tag, type_, name)
+                            tf.summary.scalar(graph_name, value.avg, step=calc_index(self, graph_name))
+                    except:
+                        ### type(losses) is numeric
+                        tf.summary.scalar(graph_name, self.logs[tag][type_].avg, step=calc_index(self, graph_name))
 
     def log(self, idx, tag):
         x = deepcopy(self.logs[tag])
